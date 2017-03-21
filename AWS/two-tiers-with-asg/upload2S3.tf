7f9a927d5c2310ad7f0adb65427d2d1bb2015a6b
Vincent Dagoury <vincent@MacBook-Pro-de-Vincent.local>
2017-03-21 13:36:27 +0100

    updating

--- AWS/two-tiers-with-asg/upload2S3.tf
+++ AWS/two-tiers-with-asg/upload2S3.tf
@@ -1,6 +1,13 @@
-resource "aws_s3_bucket_object" "object" {
+resource "aws_s3_bucket_object" "index" {
   bucket = "${var.s3_bucket}"
   key    = "index.html"
   source = "assets/index.html"
   etag   = "${md5(file("assets/index.html"))}"
+}
+#Flyer_Innovation_Day.JPG
+resource "aws_s3_bucket_object" "image" {
+  bucket = "${var.s3_bucket}"
+  key    = "Flyer_Innovation_Day.JPG"
+  source = "assets/Flyer_Innovation_Day.JPG"
+  etag   = "${md5(file("assets/Flyer_Innovation_Day.JPG"))}"
 }
